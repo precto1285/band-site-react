5d7880cc84867a8e26257fd01726462834079467
Philippe Recto <precto1285@gmal.com>
2019-11-11 06:37:33 -0800

    navbar brand

--- band-site-react/src/components/IntroImg.js
+++ band-site-react/src/components/IntroImg.js
@@ -8,14 +8,11 @@ const IntroImg = (props) => {
   return (
     <div id="introImage">
       <Row>
-        <Col className="col-lg-12 pb-3">
-
-          <center>
-            <img src={Anarchy} alt="anarchy" width="50%" />
-            <h1 id="bandName" className="my-3 text-danger">
-              Anarchy Turtle
-      </h1>
-          </center>
+        <Col className="col-lg-12 pb-3 text-center">
+          <img id="logoAnarchy" src={Anarchy} alt="anarchy" width="55%" />
+          <h1 id="bandName" className="my-3 text-danger">
+            Anarchy Turtle
+            </h1>
         </Col>
       </Row>
     </div>
--- band-site-react/src/components/NavController.js
+++ band-site-react/src/components/NavController.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import AnarchyBrand from '../images/Anarchy.png';
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
 
 const NavController = (props) => {
   return (
-    <div>NavController Component</div>
+    <Nav>
+      <NavbarBrand>
+        <a href="#">
+          <img src={AnarchyBrand} alt="anarchyBrand" width="15%" />
+        </a>
+      </NavbarBrand>
+
+    </Nav>
   );
 };
 

d3fd1bb1ff3d95c3b1218e3541cead404c3631fa
Philippe Recto <precto1285@gmal.com>
2019-11-11 06:25:43 -0800

    created intro image

--- band-site-react/src/components/IntroImg.js
+++ band-site-react/src/components/IntroImg.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import "./Components.css";
+import { Row, Col } from 'reactstrap';
+import Anarchy from '../images/Anarchy.png';
 
 const IntroImg = (props) => {
+
   return (
-    <div>IntroImg Component</div>
+    <div id="introImage">
+      <Row>
+        <Col className="col-lg-12 pb-3">
+
+          <center>
+            <img src={Anarchy} alt="anarchy" width="50%" />
+            <h1 id="bandName" className="my-3 text-danger">
+              Anarchy Turtle
+      </h1>
+          </center>
+        </Col>
+      </Row>
+    </div>
   );
 };
 

cf57e4b8f20b2b55e75e91cf44da432c97826c6a
Philippe Recto <precto1285@gmal.com>
2019-11-11 06:42:15 -0800

    created nav links

--- band-site-react/src/components/NavController.js
+++ band-site-react/src/components/NavController.js
@@ -4,14 +4,37 @@ import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
 
 const NavController = (props) => {
   return (
-    <Nav>
+
+    <Navbar>
       <NavbarBrand>
         <a href="#">
           <img src={AnarchyBrand} alt="anarchyBrand" width="15%" />
         </a>
       </NavbarBrand>
+      <Nav>
+        <NavItem>
+          <NavLink href="#">
+            The Band
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            Upcoming Shows
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            Merchandise
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            Join Us
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
 
-    </Nav>
   );
 };
 
